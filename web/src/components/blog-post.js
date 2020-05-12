5c009d09948bec338fd477ac8d6ea749e0433fdb
Yuliya Smilyanski <yule@Yuliyas-MacBook-Pro.local>
2020-05-12 09:18:17 -0400

    blog post page fixes

--- web/src/components/blog-post.js
+++ web/src/components/blog-post.js
@@ -9,39 +9,27 @@ import AuthorList from './author-list'
 import styles from './blog-post.module.css'
 
 function BlogPost (props) {
-  const {_rawBody, authors, categories, title, mainImage, publishedAt} = props
+  const {_rawBody, excerpt, categories, title, mainImage, publishedAt} = props
   return (
     <article className={styles.root}>
-      {mainImage && mainImage.asset && (
-        <div className={styles.mainImage}>
-          <img
-            src={imageUrlFor(buildImageObj(mainImage))
-              .width(1200)
-              .height(Math.floor((9 / 16) * 1200))
-              .fit('crop')
-              .auto('format')
-              .url()}
-            alt={mainImage.alt}
-          />
-        </div>
-      )}
       <Container>
         <div className={styles.grid}>
           <div className={styles.mainContent}>
             <h1 className={styles.title}>{title}</h1>
-            {_rawBody && <PortableText blocks={_rawBody} />}
-          </div>
-          <aside className={styles.metaContent}>
+            {props._rawExcerpt && (
+              <div className={styles.excerpt}>
+                <PortableText blocks={props._rawExcerpt} />
+            </div>
+            )}
             {publishedAt && (
               <div className={styles.publishedAt}>
                 {differenceInDays(new Date(publishedAt), new Date()) > 3
                   ? distanceInWords(new Date(publishedAt), new Date())
-                  : format(new Date(publishedAt), 'MMMM Do, YYYY')}
+                  : format(new Date(publishedAt), 'MMMM YYYY')}
               </div>
             )}
             {categories && (
               <div className={styles.categories}>
-                <h3 className={styles.categoriesHeadline}>Categories</h3>
                 <ul>
                   {categories.map(category => (
                     <li key={category._id}>{category.title}</li>
@@ -49,7 +37,21 @@ function BlogPost (props) {
                 </ul>
               </div>
             )}
-          </aside>
+            {mainImage && mainImage.asset && (
+              <div className={styles.mainImage}>
+                <img
+                  src={imageUrlFor(buildImageObj(mainImage))
+                    .width(1200)
+                    .height(Math.floor((9 / 16) * 1200))
+                    .fit('crop')
+                    .auto('format')
+                    .url()}
+                  alt={mainImage.alt}
+                />
+              </div>
+            )}
+            {_rawBody && <PortableText blocks={_rawBody} />}
+          </div>
         </div>
       </Container>
     </article>
